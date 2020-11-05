524c337dc7cc458ad4734c0870f7fb759181c0c8
Shayam Bhudia <2394138b@student.gla.ac.uk>
2020-11-05 11:41:42 +0100

    Mike

--- date.c
+++ date.c
@@ -1,18 +1,15 @@
-// @author Shayam N. Bhudia
 
-#include "date.h"
+
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <regex.h>
 #include <string.h>
+#include <stdlib.h>
+#include "date.h"
 
-// define the date structure
-struct date {
-    int day;  // day (2 digits)
-    int month;  // month (2 digits)
-    int year;  // year (4 digits)
-};
+/*
+* Date structure to hold date info
+*/
+struct date { int day; int month; int year; };
 
 /*
  * date_create creates a Date structure from `datestr`
@@ -20,54 +17,50 @@ struct date {
  * returns pointer to Date structure if successful,
  *         NULL if not (syntax error)
  */
-Date *date_create(char *datestr) {
+Date *date_create(char* datestr) {
+  if(strlen(datestr)==0){
+    return NULL;
+  }
+  char buffer[5] = ""; // Buffer to hold date numbers
+  int numbers[] = {0,0,0}; // Array to store date numbers
 
-	// // check for valid input format
-	// regex_t regex;
-	// int regex_compiled;
-	// int valid_date_format;
+  int buffer_counter = 0;
+  int char_counter = 0;
+  int numbers_counter = 0;
 
-	// regex_compiled = regcomp(®ex, "[:number:][:number:]/[:number:][:number:]/[:number:][:number:][:number:][:number:]", 0);
-	// valid_date_format = regexec(®ex, &datestr, 0, NULL, 0);
+  while (char_counter <= strlen(datestr)){
+    char letter = datestr[char_counter];
+    // If character is a delimiter or end of string
+    // add the number store in buffer to numbers array.
+    // Empty out the buffer
+    if(letter == '/' || letter == '\0'){
+      numbers[numbers_counter] = atoi(buffer);
+      numbers_counter = numbers_counter + 1;
+      buffer_counter = 0;
+      memset(buffer, 0, 4);
+    } else {
+      buffer[buffer_counter] = letter;
+      buffer_counter = buffer_counter + 1;
+    }
+    char_counter = char_counter + 1;
+  }
 
-	int valid_date_format = 0;
+  // Check for 0 date numbers
+  if(numbers[0] == 0 || numbers[1] == 0 || numbers[2]==0){
+    return NULL;
+  }
 
-	if (valid_date_format == 0) {
-		Date *date = (Date *)malloc(sizeof(Date));
+  // Allocate memory for Date
+  Date* x = malloc(sizeof(Date));
+  if(x){
+    x->day = numbers[0];
+    x->month = numbers[1];
+    x->year = numbers[2];
+    return x;
+  } else {
+    return NULL;
+  }
 
-		// convert the date string into integer values
-		char dd_str[3];
-		char mm_str[3];
-		char yyyy_str[5];
-
-		// slice the string date into day, month and year
-		memcpy(dd_str, &datestr[0], 2);
-		memcpy(mm_str, &datestr[3], 2);
-		memcpy(yyyy_str, &datestr[6], 4);
-
-		// convert string into integer
-		int dd, mm, yyyy;
-		sscanf(dd_str, "%d", &dd);
-		sscanf(mm_str, "%d", &mm);
-		sscanf(yyyy_str, "%d", &yyyy);
-
-		// assign values to the structure
-		date->day = dd;
-		date->month = mm;
-		date->year = yyyy;
-
-		// // test values
-		// printf("%d/", date->day);
-		// printf("%d/", date->month);
-		// printf("%d\n", date->year);
-
-		return date;
-
-	} else {
-		return NULL;
-	}
-
-	
 }
 
 /*
@@ -75,43 +68,57 @@ Date *date_create(char *datestr) {
  * returns pointer to new Date structure if successful,
  *         NULL if not (memory allocation failure)
  */
-Date *date_duplicate(Date *d) {
-	Date *date_cpy = (Date *)malloc(sizeof(Date));
-	date_cpy->day = d->day;
-	date_cpy->month = d->month;
-	date_cpy->year = d->year;
-	return date_cpy;
+Date *date_duplicate(Date *d){
+  Date* x = malloc(sizeof(Date));
+  if(x){
+  x->day = d->day;
+  x->month = d->month;
+  x->year = d->year;
+  return x;
+  } else {
+    return NULL;
+  }
 }
 
 /*
- * date_compare compares two dates, returning <0, 0, >0 if
+ * date_compare compares two dates, returning -1, 0, 1
  * date1<date2, date1==date2, date1>date2, respectively
  */
-int date_compare(Date *date1, Date *date2) {
-	if (date1->year > date2->year) {
-		return 1;
-	} else if (date1->year < date2->year) {
-		return -1;
-	} else {
-		if (date1->month > date2->month) {
-			return 1;
-		} else if (date1->month < date2->month) {
-			return -1;
-		} else {
-			if (date1->day > date2->day) {
-				return 1;
-			} else if (date1->day < date2->day) {
-				return -1;
-			} else {
-				return 0;
-			}
-		}
-	}
+int date_compare(Date *date1, Date *date2){
+  // Compare if equal
+  if(date1->day==date2->day && date1->month==date2->month && date1->year==date2->year){
+    return 0;
+  } else {
+    // Compare years
+    if(date1->year > date2->year){
+      return 1;
+    } else {
+      if(date1->year < date2->year){
+        return -1;
+      } else {
+        // Compare months
+        if(date1->month > date2->month){
+          return 1;
+        } else {
+          if(date1->month < date2->month){
+             return -1;
+          } else {
+            // Compare dates
+            if(date1->day > date2->day){
+              return 1;
+            } else {
+              return -1;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 /*
  * date_destroy returns any storage associated with `d' to the system
  */
-void date_destroy(Date *d) {
-	free(d);
+void date_destroy(Date *d){
+  free(d);
 }
